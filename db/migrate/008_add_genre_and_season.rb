b56fa68f3db5c8fa7d06b34077bd2cfcc3cc4f89
Ashley Norton <nortonashleym@gmail.com>
2020-03-01 06:09:38 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_genre_and_season.rb
+++ db/migrate/008_add_genre_and_season.rb
@@ -1,4 +1,4 @@
-class AddDay < ActiveRecord::Migration[5.1]
+class AddGenreAndSeason < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :genre, :string
     add_column :shows, :season, :string
